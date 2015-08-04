ab391850bf4554b9616dd5e5b19d6597e064bb41
Paola Socorro <pao.socorro@gmail.com>
2015-08-04 16:52:53 -0700

    Test file, with import os. Using it to get file names. has gitignore.

---
+++ test.py
@@ -0,0 +1,21 @@
+import os
+
+# root_dir = raw_input("Enter File Path: ")
+root_dir = "C:\Users\Paola\Desktop\Comic Test"
+
+# data_files = (x[0], x[2]) for x in os.walk(root_dir)]
+data_files =[]
+
+
+for root, dirs, files in os.walk(root_dir):
+    print "Root Directory: " ,root 
+    print "Directories in Root: ",dirs
+    for i in files:
+        print "A file: ",i
+    print " "
+    print "#################################"
+    print " "
+
+print type(data_files)
+
+
